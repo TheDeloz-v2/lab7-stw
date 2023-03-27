a8964aeab07a765743722f0baa85b28d4289141f
Deloz09 <84214640+TheDeloz-v2@users.noreply.github.com>
2023-03-27 17:44:44 -0600

    Update 2
    Implementación completa de Webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-        output: {
-            path: path.resolve(__dirname, 'dist_webpack'),
-            filename: '[name].bundle.js',
-        },
+    entry: {
+        index: './src/index.js',
+        populares: './src/populares.js',
+        nuevos: './src/nuevos.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist_webpack'),
+        filename: '[name].bundle.js',
+    },
 
-        module: {
-            rules: [
+
+    module: {
+        rules: [
             {
-                test: /.css$/i,
-                use: ["style-loader", "css-loader"],
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
             },
-            ],
-        },
+        ],
+    },
 };
